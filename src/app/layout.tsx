38de46788f3ffab4d4b00cfda76bebba916e0981
Fady Kuzman <fady.kuzman@pm.me>
2025-07-08 20:54:44 +0200

    Add CookieYes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,25 @@
-import '../../globals.css'
+import "../../globals.css";
 
 export const metadata = {
-  title: 'Fady Kuzman',
-  description: 'Portfolio of Fady Kuzman',
-}
+	title: "Fady Kuzman",
+	description: "Portfolio of Fady Kuzman",
+};
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  )
+	return (
+		<html lang="en">
+			<head>
+				<script
+					id="cookieyes"
+					type="text/javascript"
+					src="https://cdn-cookieyes.com/client_data/4cb95812dbde9366d2be1527/script.js"
+				></script>
+			</head>
+			<body>{children}</body>
+		</html>
+	);
 }
